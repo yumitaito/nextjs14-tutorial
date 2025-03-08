6539ec26ccb99a3997ceed320e99d6a9c9b8623a
yumitaito <ninjamankk2@yahoo.co.jp>
2025-03-08 19:17:48 +0900

    loadingページの設定

--- src/app/api/tasks/route.ts
+++ src/app/api/tasks/route.ts
@@ -10,7 +10,11 @@ const tasks: Task[] = [
   { id: 2, name: "ランニング" },
 ];
 
+const sleep = (ms: number): Promise<void> =>
+  new Promise((resolve) => setTimeout(resolve, ms));
+
 export const GET = async () => {
+  await sleep(3000);
   return NextResponse.json({ tasks }, { status: 200 });
 };
 
