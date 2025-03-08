a1d64a02a32f82c5667d178d7e54a7c76a123182
yumitaito <ninjamankk2@yahoo.co.jp>
2025-03-08 18:58:26 +0900

    クライアントコンポーネントとサーバーコンポーネントの挙動確認

---
+++ src/app/cc/page.tsx
@@ -0,0 +1,8 @@
+"use client";
+
+const ClientComponent = () => {
+  console.log("ClientComponent");
+  return <div>ClientComponent</div>;
+};
+
+export default ClientComponent;
---
+++ src/app/sc/page.tsx
@@ -0,0 +1,6 @@
+const ServerComponent = () => {
+  console.log("ServerComponent");
+  return <div>ServerComponent</div>;
+};
+
+export default ServerComponent;
